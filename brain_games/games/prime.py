9e28707892b1a459a6db6af002eb7d4ed360a495
Ilya Sachkov <sos31337@gmail.com>
2022-05-29 17:27:30 +0300

    Refactoring stage2.2

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,15 +6,17 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_question_and_answer():
     number = randint(1, 100)
     question = number
-    correct_answer = is_prime(number) and 'yes' or 'no'
+    correct_answer = is_prime(number) and 'no' or 'yes'
     return question, correct_answer
 
 
 def is_prime(a):
     b = a
+    if a <= 1:
+        return True
     for i in range(2, a):
         b -= 1
-        if a % b == 0 and a > 2:
-            return False
+        if a % b == 0:
+            return True
     else:
-        return True
+        return False
